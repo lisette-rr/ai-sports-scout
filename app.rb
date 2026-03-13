eb600b259a8d7eb115ff63cea233da78de3d9ca3
lisette-rr <lisetterealrico5001@gmail.com>
2026-03-13 14:59:12 -0600

    feat: connect web dashboard to Gemini AI scouting engine

--- app.rb
+++ app.rb
@@ -4,3 +4,7 @@ require_relative 'scout_roster'
 get '/' do
   erb :index
 end
+get '/scout' do
+    run_scout_and_media
+    "Scouting process complete. Check the scouting_reports folder and the social_media_reports folder for the new files."
+end
